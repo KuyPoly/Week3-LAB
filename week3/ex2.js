14082bb4616af3267130db68b50322eec50999ad
KuyPoly <kuypoly297@gmail.com>
2025-02-13 10:19:56 +0700

    Update ex2.js

--- week3/ex2.js
+++ week3/ex2.js
@@ -6,28 +6,38 @@
 // TODO:
 // - Update the data strucure and the functions to manage those new properties
 const STUDENTS_DATA = [
-  { firstName: "An",lastname: "lna",batch: "2027", age: 20 },
-  { firstName: "Bình",lastname: "lan",batch: "2027" , age: 22 },
-  { firstName: "Cẩm",lastname: "aln",batch: "2027" , age: 21 },
-  { firstName: "An",lastname: "laan",batch: "2027" , age: 19 }, // Duplicate first name !
+  { firstName: "An", age: 20 },
+  { firstName: "Bình", age: 22 },
+  { firstName: "Cẩm", age: 21 },
+  { firstName: "An", age: 19 }, // Duplicate first name !
 ];
-
+function updateStudentinfo(firstName, lastName, batch, age) {
+  let Student = STUDENTS_DATA.find((s) => s.firstName === firstName && s.age === age);
+  if(Student){
+    Student.lastName = lastName;
+    Student.batch = batch;
+  }
+  
+}
 /**
  * Update 1 student age, given his/her first name
  * @param {string} firstName - the student first name
- * @param {string} lastname - the student
- * @param {string} batch - the student
  * @param {age} newAge  - the student new age
  */
-function updateStudentAge(firstName,lastname, batch, newAge) {
-  let student = STUDENTS_DATA.find((s) => s.firstName === firstName && s.lastname === lastname && s.batch === batch);
+function updateStudentAge(firstName, lastName, batch, newAge) {
+
+  let student = STUDENTS_DATA.find((s) => s.firstName === firstName && s.lastName === lastName);
   if (student) {
     student.age = newAge;
   }
 }
 
 // 1 - Update An age to 30
-updateStudentAge("An","lna","2027", 30);
+updateStudentinfo("An", "Nguy", "2024", 20);
+updateStudentinfo("Bình", "Lam", "2025", 22);
+updateStudentinfo("Cẩm", "Lê", "2026", 21);
+updateStudentinfo("An", "Nguyen", "2024", 19);
+updateStudentAge("An","Nguy", "2024", 30);
 
 // 2 - Print the updated data
 console.log(JSON.stringify(STUDENTS_DATA));
